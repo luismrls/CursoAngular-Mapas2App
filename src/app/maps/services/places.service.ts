56b4c45eea2aa7dbec514686d9c3ae422a4773cd
Luis Morales <dev.luismorales@gmail.com>
2022-06-29 16:27:11 -0500

    peticion de lugares a la api de mapbox

--- src/app/maps/services/places.service.ts
+++ src/app/maps/services/places.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Feature, PlacesResponse } from '../interfaces/places.interface';
 
 @Injectable({
   providedIn: 'root'
@@ -6,12 +8,14 @@ import { Injectable } from '@angular/core';
 export class PlacesService {
 
   public useLocation?: [number, number];
+  public isLoadingPlaces: boolean = false;
+  public places: Feature[] = [];
 
   get isUserLocationReady(): boolean {
     return !!this.useLocation;
   }
 
-  constructor() {
+  constructor(private http: HttpClient) {
     this.getUserLocation();
    }
 
@@ -33,4 +37,18 @@ export class PlacesService {
       )
     })
   }
+
+  getPlaceByQuery(query: string = '') {
+
+    this.isLoadingPlaces = true;
+
+    this.http.get<PlacesResponse>(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?types=place,postcode,address&language=es&limit=1&access_token=pk.eyJ1IjoibWFnZGllbG1zIiwiYSI6ImNreXB6ZXJ1azBjeXEybnMyYm01bHk0aHAifQ.8iVI9ERFXi5ET09QbKJcww`)
+     .subscribe(response => {
+      console.log(response.features)
+      this.isLoadingPlaces = false;
+      this.places = response.features;
+     })
+  }
+
+
 }
