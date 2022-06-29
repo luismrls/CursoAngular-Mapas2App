29c06d042d43e33b6fe7138f2a566c8f2df97289
Luis Morales <dev.luismorales@gmail.com>
2022-06-29 11:45:55 -0500

    obteniendo la geolocalizacion

--- src/app/maps/screens/map-screen/map-screen.component.ts
+++ src/app/maps/screens/map-screen/map-screen.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlacesService } from '../../services';
 
 @Component({
   selector: 'app-map-screen',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapScreenComponent implements OnInit {
 
-  constructor() { }
+  constructor(private placesService: PlacesService) { }
 
   ngOnInit(): void {
   }
